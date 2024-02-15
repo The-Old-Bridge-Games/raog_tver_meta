a5173fdc2a11467fd3cf7bea3a08a8c64b8c9de9
Sovetsk1y <39431565+Sovetsk1y@users.noreply.github.com>
2024-02-15 14:00:45 +0300

    changes

--- .docs/flutter_service_worker.js
+++ .docs/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4a2638d1d11ab6c9f7d13bb99ddd35f3",
-"index.html": "672d0fb4e20ef8067091799a4adb7899",
-"/": "672d0fb4e20ef8067091799a4adb7899",
+"index.html": "38ba9457590f18f69a0c5ee1893e9794",
+"/": "38ba9457590f18f69a0c5ee1893e9794",
 "main.dart.js": "ebe1a4c8bf491182a328d8b53daaf34d",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
